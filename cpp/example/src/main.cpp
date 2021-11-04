0932ad754ff603c6757e198b1ca5564784dd26e0
zcemycl <yui.leung.15@ucl.ac.uk>
2021-11-04 00:04:25 +0000

    update

--- cpp/example/src/main.cpp
+++ cpp/example/src/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include "Formula.h"
+#include <Adder/adder.h>
+
+using namespace std;
 
 int main() {
-    std::cout << "Bla: " << Formula::bla(2) << std::endl;
+    cout << "Bla: " << Formula::bla(2) << endl;
+    cout << add(62.2f,12.2f) << endl;
     return 0;
 }
