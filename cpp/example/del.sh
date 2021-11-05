19cb75e4d21f5cdc355874311490f11361314c6d
zcemycl <yui.leung.15@ucl.ac.uk>
2021-11-05 21:05:48 +0000

    update

--- cpp/example/del.sh
+++ cpp/example/del.sh
@@ -2,5 +2,7 @@
 [[ -z "$1" ]] && { echo "Project name is empty" ; exit 1; }
 name=$1
 
-echo src/$name 
-echo tst/$name
+echo rm -r src/$name 
+rm -r src/$name 
+echo rm -r tst/$name
+rm -r tst/$name
--- cpp/example/init.sh
+++ cpp/example/init.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 [[ -z "$1" ]] && { echo "Project name is empty" ; exit 1; }
 name=$1
-echo src/"${name^}"/
-echo src/"${name^}"/"${name,}".cpp
-echo src/"${name^}"/"${name,}".h
-echo src/"${name^}"/CMakeLists.txt
+echo mkdir -p src/"${name^}"/
+mkdir -p src/"${name^}"/
+echo touch src/"${name^}"/"${name,}".cpp
+touch src/"${name^}"/"${name,}".cpp
+echo touch src/"${name^}"/"${name,}".h
+touch src/"${name^}"/"${name,}".h
+echo touch src/"${name^}"/CMakeLists.txt
+touch src/"${name^}"/CMakeLists.txt
 
-echo tst/"${name^}"/
-echo tst/"${name^}"/"${name,}"-test.cpp
+echo mkdir -p tst/"${name^}"/
+mkdir -p tst/"${name^}"/
+echo touch tst/"${name^}"/"${name^}"-test.cpp
+touch tst/"${name^}"/"${name^}"-test.cpp
