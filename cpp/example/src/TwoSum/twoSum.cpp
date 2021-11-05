1ea4884e2d832225f5b528dd7075666bd57fbce0
zcemycl <yui.leung.15@ucl.ac.uk>
2021-11-05 17:30:35 +0000

    update

---
+++ cpp/example/src/TwoSum/twoSum.cpp
@@ -0,0 +1,16 @@
+#include "twoSum.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> Solution::naive(vector<int>& nums,int target){
+
+
+}
+
+vector<int> Solution::hash(vector<int>& nums,int target){
+
+
+}
+
---
+++ cpp/example/src/TwoSum/twoSum.h
@@ -0,0 +1,8 @@
+#include <vector>
+using namespace std;
+
+class Solution{
+    public:
+        static vector<int> naive(vector<int>& nums, int target);
+        static vector<int> hash(vector<int>& nums, int target);
+};
