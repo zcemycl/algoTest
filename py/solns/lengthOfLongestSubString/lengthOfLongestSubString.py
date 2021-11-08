b5acc753a93155db9d10627cc4512ed3cacc996b
zcemycl <yui.leung.15@ucl.ac.uk>
2021-11-08 09:58:46 +0000

    add test for longest substring without repeating chars

--- py/solns/lengthOfLongestSubString/lengthOfLongestSubString.py
+++ py/solns/lengthOfLongestSubString/lengthOfLongestSubString.py
@@ -2,7 +2,14 @@ class Solution:
     @staticmethod
     def naive(string):
         maxLen = 0
-        # candidates = [set()]
-        # for c in string:
-        #     if c not in candidates
+        tmp = []
+        for i in range(len(string)):
+            if string[i] not in tmp:
+                tmp.append(string[i])
+            else:
+                tmpind = tmp.index(string[i])
+                tmp = tmp[tmpind+1:]
+                tmp.append(string[i])
+            if len(tmp) > maxLen:
+                maxLen = len(tmp)
         return maxLen
