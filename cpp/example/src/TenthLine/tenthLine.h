76d7eac34b16a5d3dd6e521707e8c29a0aa838b7
zcemycl <yui.leung.15@ucl.ac.uk>
2021-11-15 10:49:34 +0000

    try to include filesystem

--- cpp/example/src/TenthLine/tenthLine.h
+++ cpp/example/src/TenthLine/tenthLine.h
@@ -3,10 +3,10 @@
 #include <fstream>
 #include <stdio.h>
 #include <stdlib.h>
-#if __has_include(<filesystem>)
-  #include <filesystem>
-  namespace fs = std::filesystem;
-#elif __has_include(<experimental/filesystem>)
+// #if __has_include(<filesystem>)
+//   #include <filesystem>
+//   namespace fs = std::filesystem;
+#if __has_include(<experimental/filesystem>)
   #include <experimental/filesystem> 
   namespace fs = std::experimental::filesystem;
 #else
