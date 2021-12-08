047388e3d710541fb5a005baf5e68933f605f67e
zcemycl <yui.leung.15@ucl.ac.uk>
2021-12-08 10:11:59 +0000

    update

--- py/tests/testLongestCommonSubseq/test_LongestCommonSubseq.py
+++ py/tests/testLongestCommonSubseq/test_LongestCommonSubseq.py
@@ -8,13 +8,13 @@ class UnitTest_LongestCommonSubseq(unittest.TestCase):
     #     ['abcde','ace',3],['abc','def',0],
     #     ['abc','abc',3],
     #     ['zzace','abyclle',3],['abyclle','zzace',3],
-    #     ['ezupkr','ubmrapg',2],['bsbininm','jmjkbkjkv',1],
-    #     ['ylqpejqbalahwr','yrkzavgdmdgtqpg',3],
+    #     # ['ezupkr','ubmrapg',2],['bsbininm','jmjkbkjkv',1],
+    #     # ['ylqpejqbalahwr','yrkzavgdmdgtqpg',3],
     #     # ['mhunuzqrkzsnidwbun','szulspmhwpazoxijwbq',6]
     # ])
     # def test_naive(self,text1,text2,expected):
-    #     # self.assertEqual(Solution.naive(text1,text2),expected)
-    #     Solution.naive(text1,text2)
+    #     self.assertEqual(Solution.naive(text1,text2),expected)
+    #     # Solution.naive(text1,text2)
     @p.expand([
         ['abcde','ace',3],['abc','def',0],
         ['abc','abc',3],['zzace','abyclle',3],['abyclle','zzace',3],
