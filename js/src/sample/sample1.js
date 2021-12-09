e5e05c1628c192fc798079a1d4bba2f34eccfc18
zcemycl <yui.leung.15@ucl.ac.uk>
2021-12-09 14:54:17 +0000

    try js inheritance class

--- js/src/sample/sample1.js
+++ js/src/sample/sample1.js
@@ -3,16 +3,35 @@ class Solution{
         this.int1 = int1;
         this.int2 = int2;
     }
+    soln1(){
+        return this.int1+this.int2;
+    }
     static soln2(int1,int2){
         return int1+int2;
     }
 }
 
+class Solution1 extends Solution{
+    soln1(){
+        return this.int1*this.int2;
+    }
+}
+
+const testFunc = (a,b) => {
+    return a*b;
+}
 
 if (require.main === module) {
+    var s = new Solution(2,3);
+    var s1= new Solution1(2,3);
+    console.log(s.soln1());
+    console.log(s1.soln1());
     console.log(Solution.soln2(1,10));
+    console.log(testFunc(10,20));
 }
 
 module.exports = {
-    Solution:Solution
+    Solution:Solution,
+    testFunc:testFunc,
+    Solution1:Solution1,
 }
--- js/test/sample/sample1.test.js
+++ js/test/sample/sample1.test.js
@@ -1,9 +1,19 @@
-const {Solution} = require('../../src/sample/sample1');
-// const {describe} = require('mocha');
+const {Solution,testFunc,Solution1} = require('../../src/sample/sample1');
 const assert = require('assert');
 
 describe('Simple test suite:', function() {
     it('static solution soln2 adds 1 to 10', function() {
         assert(Solution.soln2(1,10) === 11);
+    });
+    it('solution soln1 adds 2 to 3', ()=>{
+        var s = new Solution(2,3);
+        assert(s.soln1() === 5);
+    });
+    it('testFunc multiply', ()=>{
+        assert(testFunc(2,10) === 20);
+    });
+    it('solution1 soln1 multiply 2 to 3', ()=>{
+        var s = new Solution1(2,3);
+        assert(s.soln1() === 6);
     });
 });
