6251a15776066529a9a91df5beeb809cccb73eee
zcemycl <yui.leung.15@ucl.ac.uk>
2021-12-20 17:15:45 +0000

    py scc

--- py/solns/stronglyConnectedComponents/stronglyConnectedComponents.py
+++ py/solns/stronglyConnectedComponents/stronglyConnectedComponents.py
@@ -1,5 +1,138 @@
 from solns.graph.graph import *
+import os
+current = os.path.dirname(os.path.realpath(__file__))
 class Solution:
-    @staticmethod
-    def naive():
-        pass
+    def __init__(self,path):
+        self.t = 0
+        self.G = Graph()
+        self.Grev = Graph()
+        self.maxNode = 0
+        with open(current+path,'r') as f:
+            for line in f.readlines():
+                u,v = line.replace('\n','').split(' ')[:2]
+                u,v = int(u),int(v)
+                self.G.addEdge(u,v)
+                self.Grev.addEdge(v,u)
+                self.maxNode = max(self.maxNode,u,v)
+               
+    def naive(self):
+        visited,n2t,t2n = set(),{},{}
+        def dfs1(i): 
+            if i in visited:
+                return 
+            visited.add(i)
+            for neighbor in self.Grev.graph[i]:
+                if neighbor not in visited:
+                    dfs1(neighbor)
+            n2t[i] = self.t
+            t2n[self.t] = i
+            self.t+=1
+        
+        for n in range(self.maxNode,-1,-1):
+            if n not in visited:
+                dfs1(n)
+
+        def dfs2(i,members): 
+            if i in visited:
+                return members
+            visited.add(i)
+            for neighbor in self.G.graph[t2n[i]]:
+                newT = n2t[neighbor]
+                if newT not in visited: 
+                    members = dfs2(newT,members+[newT])
+            return members
+
+        def dfs3(i,count):
+            if i in visited: return count
+            visited.add(i)
+            for neighbor in self.G.graph[t2n[i]]:
+                newT = n2t[neighbor]
+                if newT not in visited:
+                    count = dfs3(newT,count+1)
+            return count
+
+        print(visited,n2t,t2n)
+        visited = set()
+        for leader in range(self.maxNode,-1,-1):
+            if leader not in visited:
+                members = dfs2(leader,[leader])
+                print(members)
+        
+        visited = set()
+        for leader in range(self.maxNode,-1,-1):
+            if leader not in visited:
+                count = dfs3(leader,1)
+                print(count)
+
+    def kosarajuTwoPassRecursive(self):
+        visited,n2t,t2n = set(),{},{}
+        def dfs1(i): 
+            if i in visited:
+                return 
+            visited.add(i)
+            for neighbor in self.Grev.graph[i]:
+                if neighbor not in visited:
+                    dfs1(neighbor)
+            n2t[i] = self.t
+            t2n[self.t] = i
+            self.t+=1
+        
+        for n in range(self.maxNode,-1,-1):
+            if n not in visited:
+                dfs1(n)
+
+        def dfs3(i,count):
+            if i in visited: return count
+            visited.add(i)
+            for neighbor in self.G.graph[t2n[i]]:
+                newT = n2t[neighbor]
+                if newT not in visited:
+                    count = dfs3(newT,count+1)
+            return count
+        
+        visited = set()
+        res = []
+        for leader in range(self.maxNode,-1,-1):
+            if leader not in visited:
+                count = dfs3(leader,1)
+                res.append(count)
+        res.sort(reverse=True)
+        return res[:5]
+
+    def kosarajuTwoPassLoop(self):
+        visited,n2t,t2n = set(),{},{}
+        for n in range(self.maxNode,-1,-1):
+            if n not in visited:
+                stack = [n]
+                while stack!=[]:
+                    i = stack.pop()
+                    if i in visited:
+                        n2t[i] = self.t
+                        t2n[self.t] = i
+                        self.t+=1
+                        continue
+                    visited.add(i)
+                    stack.append(i)
+                    for neighbor in self.Grev.graph[i]:
+                        if neighbor not in visited:
+                            stack.append(neighbor)
+
+        visited = set()
+        res = []
+        for leader in range(self.maxNode,-1,-1):
+            if leader not in visited:
+                stack = [leader]
+                members,count = [],0
+                while stack!=[]:
+                    i = stack.pop()
+                    if i in visited: break
+                    visited.add(i)
+                    members.append(i)
+                    count+=1
+                    for neighbor in self.G.graph[t2n[i]]:
+                        newT = n2t[neighbor]
+                        if newT not in visited:
+                            stack.append(newT)
+                res.append(count)
+        res.sort(reverse=True)
+        return res[:5]
--- py/tests/testStronglyConnectedComponents/test_StronglyConnectedComponents.py
+++ py/tests/testStronglyConnectedComponents/test_StronglyConnectedComponents.py
@@ -5,7 +5,16 @@ from solns.stronglyConnectedComponents.stronglyConnectedComponents import *
 
 class UnitTest_StronglyConnectedComponents(unittest.TestCase):
     @p.expand([
-        []
+        ['/SCCsimple.txt',[3,3,3,1]]
     ])
-    def test_naive(self):
-        pass
+    def test_recursive(self,path,expected):
+        s = Solution(path)
+        self.assertEqual(s.kosarajuTwoPassRecursive(),expected)
+    @p.expand([
+        ['/SCCsimple.txt',[3,3,3,1]],
+        # ['/SCC.txt',[412,381,246,238,217]]
+    ])
+    def test_loop(self,path,expected):
+        s = Solution(path)
+        print(s.maxNode)
+        self.assertEqual(s.kosarajuTwoPassLoop(),expected)
