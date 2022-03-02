00d387231158d6e14cf79e8c5f77b23375d1393f
zcemycl <yui.leung.15@ucl.ac.uk>
2022-03-02 17:04:50 +0000

    py 973 k closest pts to origin

--- py/solns/kClosestPts2Origin/kClosestPts2Origin.py
+++ py/solns/kClosestPts2Origin/kClosestPts2Origin.py
@@ -1,5 +1,12 @@
-
+from heapq import heappop,heappush,heapify,heappushpop
 class Solution:
-    @staticmethod
-    def naive():
-        pass
+    def sqDist(self,point):
+        return point[0]**2+point[1]**2
+    def naive(self,points):
+        heap = [(-self.sqDist(points[i]),i) for i in range(k)]
+        heapify(heap)
+        for i in range(k,len(points)):
+            dist=-self.sqDist(points[i])
+            if dist>heap[0][0]:
+                heappushpop(heap,(dist,i))
+        return [points[i] for (_,i) in heap]        
