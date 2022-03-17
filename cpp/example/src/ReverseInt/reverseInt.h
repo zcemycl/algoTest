167ec4b9c6de0af2658be187f7a3c9dfc6afe0d5
Leo Leung <lyc010197@gmail.com>
2022-03-17 11:17:36 +0000

    fix header

--- cpp/example/src/ReverseInt/reverseInt.h
+++ cpp/example/src/ReverseInt/reverseInt.h
@@ -1,17 +1,5 @@
 #include <iostream>
 #include <math.h>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-// #include <sstream>
-// #include <filesystem>
-// #include <bits/stdc++.h>
->>>>>>> e5ed40a (try running it in macos)
-=======
->>>>>>> temp
-=======
->>>>>>> 036bb09 (update)
 
 using namespace std;
 
@@ -19,3 +7,4 @@ class reverseInt{
     public:
         static int naive(int x);
 };
+
