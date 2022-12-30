233c8efb8e8aae6aaaa4a5ffbcf4dc3e5968caa5
Yui Chun Leung <leo.leung@faculty.ai>
2022-12-30 23:01:29 +0000

    try instance2ami and ami2instance

---
+++ tf/ec2ami/modules/ami/main.tf
@@ -0,0 +1,4 @@
+resource "aws_ami_from_instance" "example" {
+    name               = var.ami_name
+    source_instance_id = var.src_instance_id
+}
---
+++ tf/ec2ami/modules/compute/vars.tf
@@ -0,0 +1,43 @@
+variable "ami" {
+    default = "ami-071f6f7c386495dc0"
+}
+
+variable "availability_zone" {
+    default = "eu-west-2a"
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+}
+
+variable "sg_name" {
+
+}
+
+variable "keypair_name" {
+    
+}
+
+variable "root_volume_size" {
+    default = 100
+}
+
+variable "root_volume_type" {
+    default = "gp2"
+}
+
+variable "user" {
+    default = "ubuntu"
+}
+
+variable "private_key" {
+    default = "mykey"
+}
+
+variable "instance_tag" {
+    default = "ec2 example terraform instance"
+}
+
+variable "is_before_ami" {
+    
+}
