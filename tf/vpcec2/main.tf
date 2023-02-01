9b97d203f5ae2f0dd32d64668f0cd7eae0e357fa
Yui Chun Leung <leo.leung@faculty.ai>
2023-02-01 13:10:01 +0000

    update

---
+++ tf/vpcec2/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = var.AWS_REGION
+}
+
+resource "aws_vpc" "vpc" {
+    cidr_block = "10.0.0.0/16"
+
+    enable_dns_support = true
+    enable_dns_hostnames = true
+
+    tags = {
+        Name = "${var.tag_author} -- ${var.tag_topic}"
+        Author = var.tag_author
+        Topic = var.tag_topic
+    }
+}
+
---
+++ tf/vpcec2/vars.tf
@@ -0,0 +1,11 @@
+variable "AWS_REGION" {
+    default = "eu-west-2"
+}
+
+variable "tag_author" {
+    default = "Leo Leung"
+}
+
+variable "tag_topic" {
+    default = "Learning and Testing"
+}
